91d69e51a3f7502bd707b89fadc9ca8b83c6bff8
Fabricio <fabricioprog@gmail.com>
2019-09-17 18:48:25 -0300

    cozinha

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -16,14 +16,11 @@
     <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets/img/icons/apple-touch-icon.png')?>">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets/img/icons/favicon-32x32.png')?>">
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/img/icons/favicon-16x16.png')?>">
-    <link rel="manifest" href="<?= base_url('assets/img/icons/site.webmanifest')?>">
+    <link rel="manifest" href="<?= base_url('assets/img/icons/manifest.json')?>">
     <link rel="mask-icon" href="<?= base_url('assets/img/icons/safari-pinned-tab.svg" color="#5bbad5')?>">
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="theme-color" content="#ffffff">
-
     <!-- FIM ícones para aplicação PWA -->
-
-
 
     <script src="<?=base_url('')?>assets/js/jquery.min.js"></script>
     <script src="<?=base_url('')?>assets/js/jquery.dataTables.min.js"></script>
